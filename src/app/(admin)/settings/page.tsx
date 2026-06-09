8acc6d82b8bf9bda2671ddc193a53388e9bd479b
davefisher813 <davefisher813@gmail.com>
2026-06-09 17:10:37 -0400

    Update page.tsx

--- src/app/(admin)/settings/page.tsx
+++ src/app/(admin)/settings/page.tsx
@@ -1,9 +1,19 @@
-import { getAssets, getServices } from "@/lib/data/resources";
+import { getAssets, getAssetTypes, getServices } from "@/lib/data/resources";
 import SettingsManager from "@/components/admin/SettingsManager";
 
 export const dynamic = "force-dynamic";
 
 export default async function SettingsPage() {
-  const [assets, services] = await Promise.all([getAssets(), getServices()]);
-  return <SettingsManager assets={assets} services={services} />;
+  const [assets, assetTypes, services] = await Promise.all([
+    getAssets(),
+    getAssetTypes(),
+    getServices(),
+  ]);
+  return (
+    <SettingsManager
+      assets={assets}
+      assetTypes={assetTypes}
+      services={services}
+    />
+  );
 }
