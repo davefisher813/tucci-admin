eba2af732bda3937eb49192981c9378530628eb5
davefisher813 <davefisher813@gmail.com>
2026-06-23 01:15:40 -0400

    Update page.tsx

--- src/app/(admin)/settings/page.tsx
+++ src/app/(admin)/settings/page.tsx
@@ -1,19 +1,26 @@
 import { getAssets, getAssetTypes, getServices } from "@/lib/data/resources";
+import { getBookingTypes } from "@/lib/data/booking-type-actions";
 import SettingsManager from "@/components/admin/SettingsManager";
+import BookingTypesManager from "@/components/admin/BookingTypesManager";
 
 export const dynamic = "force-dynamic";
 
 export default async function SettingsPage() {
-  const [assets, assetTypes, services] = await Promise.all([
+  const [assets, assetTypes, services, bookingTypes] = await Promise.all([
     getAssets(),
     getAssetTypes(),
     getServices(),
+    getBookingTypes(),
   ]);
   return (
-    <SettingsManager
-      assets={assets}
-      assetTypes={assetTypes}
-      services={services}
-    />
+    <>
+      <SettingsManager
+        assets={assets}
+        assetTypes={assetTypes}
+        services={services}
+      />
+      <BookingTypesManager types={bookingTypes} />
+    </>
   );
 }
+
