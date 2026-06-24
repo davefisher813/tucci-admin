78fce5896a1796720ae1fe84006970f11bf68085
davefisher813 <davefisher813@gmail.com>
2026-06-24 02:19:32 -0400

    Create page.tsx

---
+++ src/app/(admin)/waivers/new/page.tsx
@@ -0,0 +1,7 @@
+import WaiverIntakeForm from "@/components/admin/WaiverIntakeForm";
+
+export const dynamic = "force-dynamic";
+
+export default function NewWaiverPage() {
+  return <WaiverIntakeForm />;
+}
