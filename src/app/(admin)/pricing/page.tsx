8820a9b2ee2dbcb113c569b42a611e883ffd121e
davefisher813 <davefisher813@gmail.com>
2026-06-27 15:44:47 -0400

    Update page.tsx

--- src/app/(admin)/pricing/page.tsx
+++ src/app/(admin)/pricing/page.tsx
@@ -1,9 +1,13 @@
 import { getServices } from "@/lib/data/resources";
+import { getServiceCategories } from "@/lib/data/category-actions";
 import PricingManager from "@/components/admin/PricingManager";
 
 export const dynamic = "force-dynamic";
 
 export default async function PricingPage() {
-  const services = await getServices();
-  return <PricingManager services={services} />;
+  const [services, categories] = await Promise.all([
+    getServices(),
+    getServiceCategories(),
+  ]);
+  return <PricingManager services={services} categories={categories} />;
 }
